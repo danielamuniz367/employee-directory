3bb57636f1f4c97199d2fbfadb5db5792e76683b
Daniela Muniz <danielamuniz367@gmail.com>
2023-03-09 22:13:49 -0500

    switching to a grid page instead

--- src/components/FeaturedEmployee.tsx
+++ src/components/FeaturedEmployee.tsx
@@ -5,27 +5,31 @@ import './FeaturedEmployee.css';
 
 const FeaturedEmployee = () => {
   const { data } = useContext(Context);
-  const [featured, setFeatured] = useState<any>({});
-  const { picture, first_name, last_name, department, location } = featured;
+  // const [featured, setFeatured] = useState<any>({});
+  // const { picture, first_name, last_name, department, location } = data;
 
   useEffect(() => {
-    if (data) {
-      const randomIdx = Math.floor(Math.random() * data.length);
-      setFeatured(data[randomIdx]);
-    }
+    // if (data) {
+    //   const randomIdx = Math.floor(Math.random() * data.length);
+    //   setFeatured(data[randomIdx]);
+    // }
   }, [data]);
 
   return (
     <div className="featured-employee">
       <h4>Featured Employee</h4>
-      <CardContent>
-        <img src={picture} />
-        <div>
-          {first_name} {last_name}
-        </div>
-        <div>{department}</div>
-        <div>{location}</div>
-      </CardContent>
+      {
+        data.map((employee: any) => (
+          <CardContent>
+            <img src={employee.picture} />
+            <div>
+              {employee.first_name} {employee.last_name}
+            </div>
+            <div>{employee.department}</div>
+            <div>{employee.location}</div>
+          </CardContent>
+        ))
+      }
     </div>
   );
 };
